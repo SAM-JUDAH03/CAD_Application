3e29f43cdcd5c16436380da5e55fe26891e4ed7e
SAMJUDAH <judahsam03@gmail.com>
2026-06-15 06:36:49 +0530

    Add RelayCommand implementation for MVVM

---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Windows.Input;
+
+namespace CAD_Application.ViewModels
+{
+    /// <summary>
+    /// A generic implementation of ICommand for use in MVVM.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute(parameter);
+    }
+
+    /// <summary>
+    /// A non-generic ICommand for simple parameterless operations.
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is T typedParam)
+                return _canExecute?.Invoke(typedParam) ?? true;
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is T typedParam)
+                _execute(typedParam);
+        }
+    }
+}
